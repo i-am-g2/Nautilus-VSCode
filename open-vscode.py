8f92f033745f83581bef72c11a0207a0ef7223ee
Jeetu Kumar <33466713+i-am-g2@users.noreply.github.com>
2020-06-07 04:10:18 +0530

    Minor Update

--- open-vscode.py
+++ open-vscode.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# This example is contributed by Martin Enlund
+
 import os
 
 # A way to get unquote working with python 2 and 3
@@ -13,22 +13,22 @@ gi.require_version('Nautilus', '3.0')
 from gi.repository import Nautilus, GObject
 
 
-class OpenTerminalExtension(Nautilus.MenuProvider, GObject.GObject):
+class OpenVSCodeExtension(Nautilus.MenuProvider, GObject.GObject):
     def __init__(self):
         pass
         # self.client = GConf.Client.get_default()
         
-    def _open_terminal(self, file):
+    def _open_vscode(self, file):
         filename = unquote(file.get_uri()[7:])
 
         os.chdir(filename)
         os.system('code .')
         
     def menu_activate_cb(self, menu, file):
-        self._open_terminal(file)
+        self._open_vscode(file)
         
     def menu_background_activate_cb(self, menu, file): 
-        self._open_terminal(file)
+        self._open_vscode(file)
        
     def get_file_items(self, window, files):
         if len(files) != 1:
@@ -38,15 +38,15 @@ class OpenTerminalExtension(Nautilus.MenuProvider, GObject.GObject):
         if not file.is_directory() or file.get_uri_scheme() != 'file':
             return
        
-        item = Nautilus.MenuItem(name='NautilusPython::openterminal_file_item',
+        item = Nautilus.MenuItem(name='NautilusPython::openvscode_file_item',
                                  label='Open in VSCode' ,
-                                 tip='Open this directory in terminal %s' % file.get_name())
+                                 tip='Open %s in VSCode' % file.get_name())
         item.connect('activate', self.menu_activate_cb, file)
         return item,
 
     def get_background_items(self, window, file):
-        item = Nautilus.MenuItem(name='NautilusPython::openterminal_file_item2',
+        item = Nautilus.MenuItem(name='NautilusPython::openvscode_file_item2',
                                  label='Open in VSCode' ,
-                                 tip='Open Terminal In %s' % file.get_name())
+                                 tip='Open %s in VSCode' % file.get_name())
         item.connect('activate', self.menu_background_activate_cb, file)
         return item,
